98fdf8b47cad4ee9b21aa2234467e27d8f35796e
Kyle Marcus <kmarcus2@buffalo.edu>
2016-09-12 19:19:02 -0700

    refactor of code that finds the current line to display

--- App.jsx
+++ App.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 class App extends React.Component {
 
     constructor() {
+
         super();
         this.state = {
             line: "test"
         };
+
     }
 
     changeLine(line) {
@@ -14,6 +16,7 @@ class App extends React.Component {
     }
 
     render() {
+
         return (
            <div>
               <Header />
@@ -21,53 +24,95 @@ class App extends React.Component {
               <Footer line={this.state.line} />
            </div>
         );
+
     }
+
 }
 
 class Header extends React.Component {
+
     render() {
+
         return (
             <p>Welcome to my React text editor!</p>
         );
+
     }
+
 }
 
 class TextEditor extends React.Component {
-    handleChange(e) {
-        let start = this.refs.textEditorTextArea.selectionStart;
-        let end = start;
-        let text = e.target.value;
-        while (start >= 0) {
-            if (start == end && text[start] == '\n') start--;
-            if (text[start] != '\n') {
-                start--;
-            } else {
-                break;
-            }
-        }
-        while (end <= text.length) {
-            if (text[end] != '\n') {
-                end++;
+
+    indexAtStartOfLine(text, startIndex) {
+
+        let endIndex = startIndex;
+
+        while (startIndex >= 0) {
+            if (startIndex == endIndex && text[startIndex] == '\n') startIndex--;
+            if (text[startIndex] != '\n') {
+                startIndex--;
             } else {
                 break;
             }
         }
 
-        this.props.changeLine(text.substring(start, end));
+        return startIndex;
     }
+
+    indexAtEndOfLine(text, endIndex) {
+
+        while (endIndex <= text.length) {
+            if (text[endIndex] != '\n') {
+                endIndex++;
+            } else {
+                break;
+            }
+        }
+
+        return endIndex;
+
+    }
+
+    getLineAtCursorLocation(text, cursorIndex) {
+
+        let start = this.indexAtStartOfLine(text, cursorIndex);
+        let end = this.indexAtEndOfLine(text, cursorIndex);
+
+        return text.substring(start, end);
+
+    }
+
+    handleChange(e) {
+
+        let cursorIndex = this.refs.textEditorTextArea.selectionStart;
+        let text = e.target.value;
+
+        this.props.changeLine(this.getLineAtCursorLocation(text, cursorIndex));
+
+    }
+
     render() {
+
         return (
-            <textarea ref="textEditorTextArea" rows="50" cols="150" onKeyUp={this.handleChange.bind(this)} onMouseUp={this.handleChange.bind(this)} />
+            <textarea ref="textEditorTextArea" 
+                      rows="50" cols="150" 
+                      onKeyUp={this.handleChange.bind(this)} 
+                      onMouseUp={this.handleChange.bind(this)} />
         );
+
     }
 }
 
 class Footer extends React.Component {
+
     render() {
+
         return (
             <h1>{this.props.line}</h1>
         );
+
     }
+
 }
 
 export default App;

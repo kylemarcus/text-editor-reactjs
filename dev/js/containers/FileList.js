ade97e2bc1de25384bafa8751cd4f37bc910abae
Kyle Marcus <kmarcus2@buffalo.edu>
2016-09-23 22:32:10 -0700

    added new file modal form

--- dev/js/containers/FileList.js
+++ dev/js/containers/FileList.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {Button, ListGroup, ListGroupItem} from 'react-bootstrap';
+import {Button, FormControl, FormGroup, ListGroup, ListGroupItem, Modal} from 'react-bootstrap';
 import {selectFile} from '../actions/index';
 
 class FileList extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            showCreateNewFileModal: false
+        };
+    }
 
 	renderFileList() {
 		return this.props.files.map((file) => {
@@ -44,23 +51,50 @@ class FileList extends Component {
         }
     }
 
-    addNewFileBtnClicked() {
-        console.log("add a new file clicked");
+    toggleShowNewFileModal() {
+        this.setState(
+            {
+                showCreateNewFileModal: !this.state.showCreateNewFileModal
+            });
     }
 
     render() {
         return (
             <div>
+
                 <ListGroup>
                 	{this.renderFileList()}
                 </ListGroup>
+
                 <Button 
                     bsSize="large" 
                     block 
-                    onClick={this.addNewFileBtnClicked.bind(this)}
+                    onClick={() => this.toggleShowNewFileModal()}
                 >
                     Add a new file
                 </Button>
+
+                <Modal show={this.state.showCreateNewFileModal} onHide={() => this.toggleShowNewFileModal()}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Create new file!</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <form>
+                        <FormGroup>
+                          <FormControl
+                            type="text"
+                            placeholder="File Name"
+                            autoFocus={true}
+                          />
+                        </FormGroup>
+                      </form>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button bsStyle="primary">Create</Button>
+                    <Button onClick={() => this.toggleShowNewFileModal()}>Cancle</Button>
+                  </Modal.Footer>
+                </Modal>
+
             </div>
         );
     }
